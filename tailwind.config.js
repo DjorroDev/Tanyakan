69f7484dff855376c0710b005c03e2a02c91bab4
DjorroDev <aufarmawarid@gmail.com>
2022-06-25 15:52:39 +0700

    add plugin tailwind form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: 'class',
   content: [
@@ -35,5 +36,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
